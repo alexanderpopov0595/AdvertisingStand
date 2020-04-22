371517265beee178bec9e6025560beecfd0054f8
alexanderpopov0595 <alexanderpopov0595@gmail.com>
2020-04-22 14:10:43 +0300

    Initial commit

---
+++ src/main/java/com/tsystems/javaschool/advertstand/controller/HomeBean.java
@@ -0,0 +1,78 @@
+package com.tsystems.javaschool.advertstand.controller;
+
+import com.tsystems.javaschool.advertstand.messages.MessageReceiver;
+import org.apache.log4j.Logger;
+import javax.faces.bean.ApplicationScoped;
+import javax.inject.Named;
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.*;
+
+/**
+ * Represents a web socket server endpoint that notifies clients asynchronously with incoming JMS messages
+ */
+@Named
+@ApplicationScoped
+@ServerEndpoint(value = "/home")
+public class HomeBean implements Serializable {
+
+    private static final org.apache.log4j.Logger logger = Logger.getLogger(HomeBean.class);
+
+    /**
+     *  Sessions object holds all web socket sessions connected to this web socket server endpoint
+     */
+    private static final Set<Session> sessions = Collections.synchronizedSet(new HashSet<Session>());
+
+    /**
+     * Message object stores recieved message before the new one comes
+     */
+    private static String message;
+
+    public HomeBean(){
+
+    }
+
+    /**
+     * Method opens web socket connection and sends message to frontend
+     * @param peer -  connected user
+     */
+    @OnOpen
+    public void onOpen (Session peer)  {
+       sessions.add(peer);
+        try {
+            peer.getBasicRemote().sendText(this.message);
+        } catch (Exception e) {
+            logger.error("Error while sending stored message to web socket: "+e.getMessage());
+        }
+    }
+
+    /**
+     * Method closes connection for current user
+     * @param peer - current user
+     */
+    @OnClose
+    public void onClose (Session peer) {
+        sessions.remove(peer);
+    }
+
+    /**
+     * Method saves new received jms message and then sends this message to all open web sockets
+     * @param message
+     */
+    public void sendMessageToSocket(String message)  {
+        this.message = message;
+        for (Session session : sessions) {
+            if (session.isOpen()) {
+                try {
+                    session.getBasicRemote().sendText(message);
+                } catch (IOException e) {
+                    logger.error("Error while sending message to web socket: "+ e.getMessage());
+                }
+            } else {
+                sessions.remove(session);
+            }
+        }
+    }
+}
---
+++ src/main/java/com/tsystems/javaschool/advertstand/messages/MessageReceiver.java
@@ -0,0 +1,57 @@
+package com.tsystems.javaschool.advertstand.messages;
+
+import com.tsystems.javaschool.advertstand.controller.HomeBean;
+import org.apache.log4j.Logger;
+import org.jboss.ejb3.annotation.ResourceAdapter;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.jms.*;
+import java.io.IOException;
+
+/**
+ * Class is responsible for receiving jms messages from topic and pushing them to endpoint
+ */
+@Named
+@MessageDriven(name = "top-topic", mappedName = "jms/top-topic", activationConfig = {
+        @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Topic"),
+        @ActivationConfigProperty(propertyName = "destination", propertyValue = "top-topic"),
+        @ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge")})
+@ResourceAdapter(value = "activemq-rar.rar")
+public class MessageReceiver implements MessageListener {
+
+    private static final org.apache.log4j.Logger logger = Logger.getLogger(MessageReceiver.class);
+
+    public MessageReceiver() {
+
+    }
+
+    /**
+     * Injected endpoint to push jms messages in
+     */
+    private HomeBean endpoint;
+
+    @Inject
+    public MessageReceiver(HomeBean endpoint) {
+        this.endpoint = endpoint;
+    }
+
+    /**
+     * Method listens to jms messages and push message's text to enpoint
+     *
+     * @param message
+     */
+    @Override
+    public void onMessage(Message message) {
+        if (message instanceof TextMessage) {
+            TextMessage textMessage = (TextMessage) message;
+            try {
+                endpoint.sendMessageToSocket(textMessage.getText());
+            } catch (JMSException e) {
+                logger.error("Error receiving JMS message: " + e.getMessage());
+            }
+        }
+    }
+}
